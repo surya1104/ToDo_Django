e246daf10614858cd989e646d8b082b3556f41ef
surya <suryasnavuru@gmail.com>
2023-08-19 00:07:31 +0530

    TODO APP Completed

--- todo_task/views.py
+++ todo_task/views.py
@@ -16,8 +16,29 @@ def mark_as_done(request, pk):
     task.save()
     return redirect('home')
 
+
 def mark_as_undone(request, pk):
     task = get_object_or_404(Task, pk=pk)
     task.is_completed = False
     task.save()
+    return redirect('home')
+
+
+def edit_task(request, pk):
+    get_task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        new_task = request.POST['task']
+        get_task.task = new_task
+        get_task.save()
+        return redirect('home')
+    else:
+        context = {
+            'get_task': get_task
+        }
+    return render(request, 'editTask.html', context)
+
+
+def delete_task(request, pk):
+    get_task_dltb = get_object_or_404(Task, pk=pk)
+    get_task_dltb.delete()
     return redirect('home')
